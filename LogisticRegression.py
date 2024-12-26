f7adced68c7755ba633b2ca5644b135eb6a30592
YiyuanLei <yyloliver@gmail.com>
2024-12-26 16:21:52 -0500

    V2

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,26 +1,30 @@
 import numpy as np
 
 class LogisticRegression:
-    def __init__(self, learning_rate, epoch):
-        self.learning_rate  = learning_rate
-        self.epoch = epoch
+    def __init__(self, lr = 0.001, epoch = 1000):
+        self.lr  = lr
+        self.num_iterations = epoch
+        self.weights = None
+        self.bias = None
 
+    def cross_entropy_loss(self, y, y_hat):
+        return -(y * np.log(y_hat) + (1-y)*np.log(1-y_hat)).mean()
     def sigmoid(self, x):
         return np.exp(x)/ (1 +  np.exp(x))
     def fit(self, x, y):
         num_samples, num_features = x.shape()
-        self.W, self.b = np.zeros(num_features, 1), np.zeros(num_samples, 1)
+        self.weights, self.bias = np.zeros(num_features), 0
 
-        for _ in range(self.epoch):
-            f_x = np.dot(x.T, self.W) + self.b # (n, 1)
-            h = self.sigmoid(f_x) # (n, 1)
-            dW = (1/num_features) * np.dot(y, (x -f_x))
-            db = (1/num_features) * np.sum (h - y)
+        for _ in range(self.num_iterations):
+            prediction = self.sigmoid(x.T.dot(self.weights) + self.bias) # (n, 1)
+            error = prediction - y # (n, 1)
+            d_weights = x.T.dot(error)/num_samples
+            d_bias = error.sum()/num_samples
 
-            self.W = +- self.learning_rate * dW
-            self.b = +- self.learning_rate * db
+            self.weights -= self.lr *  d_weights
+            self.bias -= self.lr * d_bias
 
-        return self.W,self.b
+        return self.weights,self.b
     def predict(self, x):
-        prediction = self.sigmoid(np.dot(x.T, self.W) + self.b)
+        prediction = self.sigmoid(np.dot(x.T, self.weights) + self.bias)
         return prediction
